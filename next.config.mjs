a490c70d2e60710cfe33e301e87daba402a86b42
Luxury925 <ayoubghrairi.dev@gmail.com>
2026-03-11 18:10:34 +0100

    Ready for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // GitHub Pages configuration
-  basePath: '/saas-marketplace',
-  trailingSlash: true,
+ 
   output: 'export',
   images: {
     unoptimized: true,
