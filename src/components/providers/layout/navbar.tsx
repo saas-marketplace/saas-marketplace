0145b946b227d26ad2ada4bc315d7530181654ef
Luxury925 <ayoubghrairi.dev@gmail.com>
2026-03-12 16:43:58 +0100

    Fix dashboard CSS issues

--- src/components/providers/layout/navbar.tsx
+++ src/components/providers/layout/navbar.tsx
@@ -139,7 +139,6 @@ function NavbarContent() {
               transition={{ duration: 0.3 }}
               className="w-8 h-8 rounded-lg gradient-bg flex items-center justify-center"
             >
-              <Sparkles className="w-5 h-5 text-white" />
             </motion.div>
             <span className="text-xl font-bold gradient-text">
             </span>
